c267e59f3cf238395ccaa3f775071b265b8e60a3
yogevsh <yogev_shamay@walla.com>
2013-07-10 01:13:11 +0000

    Updated grader and checks

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URLFILE_DEFAULT = "null";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -59,14 +60,22 @@ var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
-};
+}
 
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_file>', 'Url to index.html', URLFILE_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+		 
+    var file = program.file;
+    if(program.url != URLFILE_DEFAULT) {
+          console.log("URL: "+program.url);
+          file = "web.js";
+    } 
+		
+    var checkJson = checkHtmlFile(file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
